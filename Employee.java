452608f786bbcddd5733fae8a65f3552723581b3
Zhaituona <42330994+Zhaituona@users.noreply.github.com>
2018-09-25 14:33:05 -0400

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,13 @@
+package StaticMethod;
+
+public class Employee {
+	
+	public static void empSalary() {
+		System.out.println("Employee salary is 90K");
+	}
+	
+	public void empVacation() {
+		System.out.println("Employee has 2 weeks vacation");
+	}
+
+}
