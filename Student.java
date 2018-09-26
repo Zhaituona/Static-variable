89e95a22c09000459ff44eab48d6865cb63f8b3d
Zhaituona <42330994+Zhaituona@users.noreply.github.com>
2018-09-25 22:06:34 -0400

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+package StaticMethod;
+
+public class Student {
+	
+	public static void studentID() {
+		System.out.println("Student ID coming from student name");
+	}
+	
+	public void studentAge() {
+		System.out.println("Student age is different");
+	}
+
+}
